5665e858e26ab50346ef9c7da2070ef9bfae3565
ssskeyi <140376562+ssskeyi@users.noreply.github.com>
2024-01-23 20:35:00 +0800

    dress.vue

--- src/pages/dress.vue
+++ src/pages/dress.vue
@@ -1,3 +1,88 @@
 <template>
-  梳妆
+  <div class="makeup-room">
+    <div class="background"></div>
+    <img class="character" :src="characterImage" alt="Character">
+    
+    <div class="buttons">
+      <button @click="changeDecoration"><img src="decorationIcon" alt="Decoration"></button>
+      <button @click="changeClothes"><img src="clothesIcon" alt="Clothes"></button>
+      <button @click="changePants"><img src="pantsIcon" alt="Pants"></button>
+      <button @click="changeShoes"><img src="shoesIcon" alt="Shoes"></button>
+    </div>
+    
+    <button class="music-button" @click="toggleMusic"><img src="musicIcon" alt="Music"></button>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      characterImage: 'path/to/defaultCharacterImage.jpg',
+      decorationIcon: 'path/to/decorationIcon.png',
+      clothesIcon: 'path/to/clothesIcon.png',
+      pantsIcon: 'path/to/pantsIcon.png',
+      shoesIcon: 'path/to/shoesIcon.png',
+      musicIcon: 'path/to/musicIcon.png',
+      isMusicPlaying: false
+    };
+  },
+  methods: {
+    changeDecoration() {
+      // 修改人物的头饰装扮
+      // 更新characterImage的值为新的头饰图片路径
+    },
+    changeClothes() {
+      // 修改人物的衣服装扮
+      // 更新characterImage的值为新的衣服图片路径
+    },
+    changePants() {
+      // 修改人物的裤子装扮
+      // 更新characterImage的值为新的裤子图片路径
+    },
+    changeShoes() {
+      // 修改人物的鞋子装扮
+      // 更新characterImage的值为新的鞋子图片路径
+    },
+    toggleMusic() {
+      // 切换音乐的播放状态
+      // 更新isMusicPlaying的值
+    }
+  }
+};
+</script>
+
+<style scoped>
+.makeup-room {
+  position: relative;
+  /* 设置背景图样式 */
+}
+
+.background {
+  /* 设置背景图样式 */
+}
+
+.character {
+  position: absolute;
+  /* 设置人物图样式 */
+}
+
+.buttons {
+  position: absolute;
+  left: 20px;
+  top: 50%;
+  transform: translateY(-50%);
+  /* 设置按钮样式 */
+}
+
+button {
+  /* 设置按钮样式 */
+}
+
+.music-button {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  /* 设置音乐按钮样式 */
+}
+</style>
