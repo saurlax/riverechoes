b7376f593b546f5c5cbaee513e8e4db4734f37c7
Saurlax <saurlax@qq.com>
2024-02-13 14:45:29 +0800

    feat: add skybox

--- src/pages/vr.vue
+++ src/pages/vr.vue
@@ -4,9 +4,10 @@ import 'aframe'
 
 <template>
   <a-scene v-pre>
-    <a-entity gltf-model="url(/models/jar.glb)" position="0 -3 -5" rotation="-90 0 0" scale="0.05 0.05 0.05"></a-entity>
-    <a-entity gltf-model="url(/models/claw.glb)" position="-5 0 0" rotation="0 0 0" scale="1 1 1"></a-entity>
-    <a-entity gltf-model="url(/models/conch.glb)" position="0 0 5" rotation="0 0 0" scale="0.05 0.05 0.05"></a-entity>
-    <a-entity gltf-model="url(/models/fossil.glb)" position="5 0 0" rotation="0 -90 0" scale="0.5 0.5 0.5"></a-entity>
+    <a-sky src="url(/assets/sky.jpg)"></a-sky>
+    <a-entity gltf-model="url(/assets/jar.glb)" position="0 -3 -5" rotation="-90 0 0" scale="0.05 0.05 0.05"></a-entity>
+    <a-entity gltf-model="url(/assets/claw.glb)" position="-5 0 0" rotation="0 0 0" scale="1 1 1"></a-entity>
+    <a-entity gltf-model="url(/assets/conch.glb)" position="0 0 5" rotation="0 0 0" scale="0.05 0.05 0.05"></a-entity>
+    <a-entity gltf-model="url(/assets/fossil.glb)" position="5 0 0" rotation="0 -90 0" scale="0.5 0.5 0.5"></a-entity>
   </a-scene>
 </template>
