9cb301a9c99d3de2ecadf842cc71b3cbc59929bf
Saurlax <saurlax@qq.com>
2024-02-15 17:06:15 +0800

    feat: update index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="background">
-    <img src="/assets/lhmy.svg" />
     <RouterLink to="/map"><button>开始游戏</button></RouterLink>
+    <div class="copy">2024 © 大连理工大学软件学院、大连理工大学-立命馆大学国际信息与软件学院赴吉林省四平市“石榴花开，籽籽同心”寒假社会实践团</div>
   </div>
 </template>
 
 <style scoped>
 .background {
   background-image: url('/assets/index.webp');
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
 }
 
-img {
-  width: min(80%, 400px);
-  filter: drop-shadow(0 0 8px #212121);
+a {
+  position: fixed;
+  bottom: 20%;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.copy {
+  font-size: 0.6rem;
+  position: fixed;
+  bottom: 0;
+  margin: 6px;
+  text-align: center;
 }
 </style>
