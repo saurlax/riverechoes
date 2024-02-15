3ac0161a85341797a9f1ce07f83089cb4970e996
Saurlax <saurlax@qq.com>
2024-02-15 18:38:32 +0800

    feat: update sacrifice

--- src/pages/sacrifice.vue
+++ src/pages/sacrifice.vue
@@ -1,11 +1,46 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+const state = reactive({
+  time: 0,
+  fire: 0
+})
+
+const change = (k: keyof typeof state) => {
+  const max = { time: 1, fire: 2 }
+  if (state[k] === max[k]) {
+    state[k] = 0
+  } else {
+    state[k]++
+  }
+}
+</script>
 <template>
-  <div class="background">
+  <div class="background" :style="`background-image: url('/assets/sacrifice/time${state.time}.webp')`">
     <RouterLink to="/map"><button>返回</button></RouterLink>
+    <button @click="change('time')">切换时间</button>
+    <button @click="change('fire')">切换火焰</button>
+    <div class="text">
+      萨满祭祀是满族文化活动，包括火祭、学祭等形式。在祭祀中，萨满通过乐器、拍掌声、歌舞向神灵祈请降临，同时娱乐神灵和人们。歌词沟通人神关系，舞蹈表现萨满的虔诚与斗争。祭祀中设供进牲，献酒，擎神刀祷祝，歌舞热烈，达到请神以祈福佑的目的。
+    </div>
+    <div class="fire" :style="`background-image: url('/assets/sacrifice/fire${state.fire}.webp')`"></div>
   </div>
 </template>
 
 <style scoped>
-.background {
-  background-image: url('/assets/sacrifice.webp');
+.fire {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  pointer-events: none;
+}
+
+.text {
+  background: #ffffff88;
+  position: fixed;
+  bottom: 0;
 }
 </style>
--- src/pages/vr.vue
+++ src/pages/vr.vue
@@ -3,11 +3,21 @@ import 'aframe'
 </script>
 
 <template>
+  <RouterLink to="/exhibition"><button>返回</button></RouterLink>
   <a-scene v-pre>
     <a-sky src="url(/assets/exhibition/sky.webp)"></a-sky>
-    <a-entity gltf-model="url(/assets/exhibition/jar.glb)" position="0 -3 -5" rotation="-90 0 0" scale="0.05 0.05 0.05"></a-entity>
-    <a-entity gltf-model="url(/assets/exhibition/claw.glb)" position="-5 0 0" rotation="0 0 0" scale="1 1 1"></a-entity>
-    <a-entity gltf-model="url(/assets/exhibition/conch.glb)" position="0 0 5" rotation="0 0 0" scale="0.05 0.05 0.05"></a-entity>
-    <a-entity gltf-model="url(/assets/exhibition/fossil.glb)" position="5 0 0" rotation="0 -90 0" scale="0.5 0.5 0.5"></a-entity>
+    <a-entity gltf-model="url(/assets/exhibition/jar.glb)" position="0 -3 -5" rotation="-90"
+      scale="0.05 0.05 0.05"></a-entity>
+    <a-entity gltf-model="url(/assets/exhibition/claw.glb)" position="-5 0 0" scale="1 1 1"></a-entity>
+    <a-entity gltf-model="url(/assets/exhibition/conch.glb)" position="0 0 5" scale="0.05 0.05 0.05"></a-entity>
+    <a-entity gltf-model="url(/assets/exhibition/fossil.glb)" position="5" rotation="0 -90"
+      scale="0.5 0.5 0.5"></a-entity>
   </a-scene>
 </template>
+
+<style scoped>
+a {
+  position: relative;
+  z-index: 100;
+}
+</style>
