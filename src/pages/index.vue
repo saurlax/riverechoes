1cc0328dc00f6a3aecb012a64a9bb6b3ae103c1a
Saurlax <saurlax@qq.com>
2024-02-16 11:34:51 +0800

    feat: update start button

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="background">
     <RouterLink to="/map"><button>开始游戏</button></RouterLink>
-    <div class="copy">2024 © 大连理工大学软件学院、大连理工大学-立命馆大学国际信息与软件学院赴吉林省四平市“石榴花开，籽籽同心”寒假社会实践团</div>
+    <div class="copy">© 2024 大连理工大学软件学院、大连理工大学-立命馆大学国际信息与软件学院赴吉林省四平市“石榴花开，籽籽同心”寒假社会实践团</div>
   </div>
 </template>
 
@@ -10,11 +10,22 @@
   background-image: url('/assets/index.webp');
 }
 
+button {
+  padding: 30px 50px;
+  background-color: initial;
+  background-image: url('/assets/start.webp');
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+
 a {
   position: fixed;
+  width: 100%;
   bottom: 20%;
-  left: 50%;
-  transform: translateX(-50%);
+  display: flex;
+  justify-content: center;
 }
 
 .copy {
