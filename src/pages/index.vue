774f007b575005af2e9dc8b3dd2e49bcd6b40687
Saurlax <saurlax@qq.com>
2024-04-21 14:31:03 +0800

    chore: remove copy

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="background">
     <RouterLink to="/map"><button>开始游戏</button></RouterLink>
-    <div class="copy">© 2024 大连理工大学软件学院、大连理工大学-立命馆大学国际信息与软件学院赴吉林省四平市“石榴花开，籽籽同心”寒假社会实践团</div>
+    <!-- <div class="copy">© 2024 大连理工大学软件学院、大连理工大学-立命馆大学国际信息与软件学院赴吉林省四平市“石榴花开，籽籽同心”寒假社会实践团</div> -->
   </div>
 </template>
 
