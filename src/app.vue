6904371a2aecbffafff4c3025cfa10fac9364c2e
Saurlax <saurlax@qq.com>
2024-06-24 11:58:10 +0800

    chore: remove stats and copy

--- src/app.vue
+++ src/app.vue
@@ -1,3 +1,13 @@
 <template>
-  <RouterView />
+  <div class="wrapper">
+    <RouterView />
+  </div>
 </template>
+
+<style scoped>
+.wrapper {
+  position: relative;
+  max-width: 60vh;
+  margin: auto;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,9 @@
+<script setup lang="ts">
+</script>
+
 <template>
   <div class="background">
     <RouterLink to="/map"><button>开始游戏</button></RouterLink>
-    <!-- <div class="copy">© 2024 大连理工大学软件学院、大连理工大学-立命馆大学国际信息与软件学院赴吉林省四平市“石榴花开，籽籽同心”寒假社会实践团</div> -->
   </div>
 </template>
 
@@ -19,20 +21,11 @@ button {
   background-repeat: no-repeat;
 }
 
-
 a {
-  position: fixed;
+  position: absolute;
   width: 100%;
   bottom: 20%;
   display: flex;
   justify-content: center;
-}
-
-.copy {
-  font-size: 0.6rem;
-  position: fixed;
-  bottom: 0;
-  margin: 6px;
-  text-align: center;
 }
 </style>
