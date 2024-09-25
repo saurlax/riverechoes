8a9cd410c6be02d4aba5986b281d13342dfdc431
Saurlax <saurlax@qq.com>
2024-09-25 21:06:35 +0800

    feat: chat api

---
+++ server/api/chat.ts
@@ -0,0 +1,5 @@
+export default defineEventHandler(async (event) => {
+  // TODO: Implement chat functionality
+  // https://platform.moonshot.cn/docs/api/chat
+  return { hello: "riverechoes" };
+});
